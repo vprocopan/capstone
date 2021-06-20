8f9c954424f934e3e3f9fbba3b09bc1738d5c879
vprocopan <vprocopan@gmail.com>
2021-06-20 22:53:56 +0300

    changed region

--- Infrastructure/create.sh
+++ Infrastructure/create.sh
@@ -2,5 +2,5 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
---region eu-west-1 \
+--region us-west-2 \
 --capabilities CAPABILITY_NAMED_IAM
